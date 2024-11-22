ce96afa2b50acc18211f9e5decf8f9c9c5281a16
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2024-11-22 12:29:28 +0100

    skycam

--- thaao_skycam.py
+++ thaao_skycam.py
@@ -22,32 +22,56 @@ __status__ = "Research"
 __lastupdate__ = "October 2024"
 
 import os
+import shutil
+import zipfile
 
 import pandas as pd
 
 import settings as ts
 import tools as tls
 
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    # for root, dirs, files in os.walk(path):
+    files = os.listdir(path)
+    for file in files:
+        ziph.write(
+                os.path.join(path, file), os.path.relpath(os.path.join(path, file), os.path.join(path, '..')))
+
+
 instr = 'skycam'
-date_list_old = pd.date_range(
-        ts.instr_metadata[instr]['start_instr'], ts.instr_metadata[instr]['end_instr'], freq='5 min').tolist()
-date_list_new = pd.date_range(
+date_list = pd.date_range(
         ts.instr_metadata[instr]['start_instr'], ts.instr_metadata[instr]['end_instr'], freq='D').tolist()
 folder = os.path.join(ts.basefolder, "thaao_" + instr)
 
 if __name__ == "__main__":
 
+    for i in date_list:
+        fn = os.path.join(folder, i.strftime('%Y'), i.strftime('%m'), i.strftime('%d'))
+        fn_new = os.path.join(folder, i.strftime('%Y'), i.strftime('%m'), i.strftime('%Y%m%d'))
+        try:
+            shutil.copytree(fn, fn_new)
+        except FileNotFoundError as e:
+            print(e)
+            continue
+
+        try:
+            with zipfile.ZipFile(
+                    os.path.join(folder, i.strftime('%Y'), i.strftime('%Y%m%d') + '.zip'), 'w',
+                    zipfile.ZIP_DEFLATED) as zipf:
+                zipdir(fn_new, zipf)
+            print(f'zipped {fn_new}')
+            try:
+                shutil.rmtree(fn_new)
+            except FileNotFoundError as e:
+                print(e)
+        except:
+            print(f'error in zipping file {fn_new}')
+
     # TODO: rimuovere "*_stack.jpg", "*_sod.jpg",
     skycam = pd.DataFrame(columns=['dt', 'mask'])
-    for i in date_list_old:
-        while i.strftime('%Y') < 2024:
-            fn = os.path.join(
-                    folder, i.strftime('%Y'), i.strftime('%m'), i.strftime('%d'),
-                    i.strftime('%Y%m%d_%H%M_raw') + ".jpg")
-            if os.path.exists(fn):
-                print(fn)
-                skycam.loc[i] = [i, True]
-    for i in date_list_old:
+    for i in date_list:
         while i.strftime('%Y') >= 2024:
             # TODO: unzip daily folders and check the content
             fn = os.path.join(
