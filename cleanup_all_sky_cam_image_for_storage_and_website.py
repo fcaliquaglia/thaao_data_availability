ec3a4bf38194fd025e3402b5cea01e1c0b1bf768
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2024-12-17 10:42:30 +0100

    readme updates

--- cleanup_all_sky_cam_image_for_storage_and_website.py
+++ cleanup_all_sky_cam_image_for_storage_and_website.py
@@ -36,7 +36,7 @@ WEB_base_folder = 'Moonglow'
 
 instr = 'skycam'
 
-year = 2023
+year = 2022
 
 date_list_upload = pd.date_range(dt.datetime(year, 1, 1), dt.datetime(year, 12, 31), freq='D').tolist()
 date_list_zip = pd.date_range(dt.datetime(year, 1, 1), dt.datetime(year, 12, 31), freq='D').tolist()
