f27f8a731d3947366be06df9d1ee6faba2566b68
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-02-26 10:36:31 +0100

    metar progress bar

--- single_instr_data_avail/metar.py
+++ single_instr_data_avail/metar.py
@@ -1,17 +1,75 @@
+# #!/usr/local/bin/python3
+# # -*- coding: utf-8 -*-
+# # -------------------------------------------------------------------------------
+# #
+# """
+# OK
+# """
+#
+# # =============================================================
+# # CREATED:
+# # AFFILIATION: INGV
+# # AUTHORS: Filippo Cali' Quaglia
+# # =============================================================
+# #
+# # -------------------------------------------------------------------------------
+# __author__ = "Filippo Cali' Quaglia"
+# __credits__ = ["??????"]
+# __license__ = "GPL"
+# __version__ = "1.1"
+# __email__ = "filippo.caliquaglia@ingv.it"
+# __status__ = "Research"
+# __lastupdate__ = "February 2025"
+#
+# instr = 'metar'
+#
+#
+# def update_data_avail(instr):
+#     import os
+#
+#     import pandas as pd
+#     import single_instr_data_avail.tools as sida_tls
+#     from urllib.request import urlopen
+#
+#     import numpy as np
+#     import settings as ts
+#
+#     folder = os.path.join(ts.basefolder, "thaao_" + instr)
+#
+#     url = (
+#             'https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station=BGTL&data=all&year1=1928&month1=1&day1=1&year2=' + str(
+#             ts.instr_metadata[instr]['end_instr'].year) + '&month2=' + str(
+#             ts.instr_metadata[instr]['end_instr'].month) + '&day2=' + str(
+#             ts.instr_metadata[instr][
+#                 'end_instr'].day) + '&tz=Etc%2FUTC&format=onlycomma&latlon=no&elev=no&missing=M&trace=T&direct=no&report_type=3&report_type=4')
+#
+#     print(url)
+#     response = urlopen(url, timeout=10000)
+#
+#     with open(os.path.join(folder, 'BGTL_METAR.csv'), 'wb') as f:
+#         f.write(response.read())
+#
+#     historical_data_all = pd.read_csv(os.path.join(folder, 'BGTL_METAR.csv'), low_memory=False, index_col='valid')
+#     historical_data_metar = historical_data_all['metar']
+#     del historical_data_all
+#
+#     vals = np.repeat(True, len(historical_data_metar))
+#     metar = pd.concat([pd.Series(historical_data_metar.index), pd.Series(vals)], axis=1)
+#     sida_tls.save_txt(instr, metar)
+
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
-# -------------------------------------------------------------------------------
-#
+
 """
 OK
 """
 
 # =============================================================
-# CREATED: 
+# CREATED:
 # AFFILIATION: INGV
 # AUTHORS: Filippo Cali' Quaglia
 # =============================================================
-#
+
 # -------------------------------------------------------------------------------
 __author__ = "Filippo Cali' Quaglia"
 __credits__ = ["??????"]
@@ -21,38 +79,53 @@ __email__ = "filippo.caliquaglia@ingv.it"
 __status__ = "Research"
 __lastupdate__ = "February 2025"
 
-instr = 'metar'
+import os
+import pandas as pd
+import numpy as np
+import settings as ts
+import single_instr_data_avail.tools as sida_tls
+from urllib.request import urlopen
+from tqdm import tqdm  # Progress bar
+import shutil  # To fetch content-length for progress bar
+
+instr = "metar"
 
 
 def update_data_avail(instr):
-    import os
+    """Updates data availability for the given instrument with a progress bar."""
 
-    import pandas as pd
-    import single_instr_data_avail.tools as sida_tls
-    from urllib.request import urlopen
-
-    import numpy as np
-    import settings as ts
-
-    folder = os.path.join(ts.basefolder, "thaao_" + instr)
+    folder = os.path.join(ts.basefolder, f"thaao_{instr}")
+    end_instr = ts.instr_metadata[instr]["end_instr"]
 
     url = (
-            'https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station=BGTL&data=all&year1=1928&month1=1&day1=1&year2=' + str(
-            ts.instr_metadata[instr]['end_instr'].year) + '&month2=' + str(
-            ts.instr_metadata[instr]['end_instr'].month) + '&day2=' + str(
-            ts.instr_metadata[instr][
-                'end_instr'].day) + '&tz=Etc%2FUTC&format=onlycomma&latlon=no&elev=no&missing=M&trace=T&direct=no&report_type=3&report_type=4')
+        f"https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
+        f"station=BGTL&data=all&year1=1928&month1=1&day1=1"
+        f"&year2={end_instr.year}&month2={end_instr.month}&day2={end_instr.day}"
+        f"&tz=Etc%2FUTC&format=onlycomma&latlon=no&elev=no&missing=M&trace=T&direct=no"
+        f"&report_type=3&report_type=4"
+    )
 
-    print(url)
-    response = urlopen(url, timeout=10000)
+    print(f"Fetching data from: {url}")
 
-    with open(os.path.join(folder, 'BGTL_METAR.csv'), 'wb') as f:
-        f.write(response.read())
+    file_path = os.path.join(folder, "BGTL_METAR.csv")
 
-    historical_data_all = pd.read_csv(os.path.join(folder, 'BGTL_METAR.csv'), low_memory=False, index_col='valid')
-    historical_data_metar = historical_data_all['metar']
-    del historical_data_all
+    try:
+        with urlopen(url, timeout=100) as response:
+            total_size = response.length  # Get file size from HTTP response
+            chunk_size = 1024 * 1024  # 1MB chunks
 
-    vals = np.repeat(True, len(historical_data_metar))
-    metar = pd.concat([pd.Series(historical_data_metar.index), pd.Series(vals)], axis=1)
+            with open(file_path, "wb") as f, tqdm(
+                total=total_size, unit="B", unit_scale=True, desc="Downloading"
+            ) as pbar:
+                for chunk in iter(lambda: response.read(chunk_size), b""):
+                    f.write(chunk)
+                    pbar.update(len(chunk))
+
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return
+
+    historical_data = pd.read_csv(file_path, usecols=["valid", "metar"], index_col="valid", low_memory=False)
+    metar = pd.DataFrame({"timestamp": historical_data.index, "available": np.ones(len(historical_data), dtype=bool)})
+
     sida_tls.save_txt(instr, metar)
