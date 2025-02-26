9644e70a70b4bd5bd1da5e1f9d42842d752c2f2b
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-02-26 09:46:03 +0100

    pm10 updates

--- single_instr_data_avail/thaao_pm10.py
+++ single_instr_data_avail/thaao_pm10.py
@@ -23,26 +23,22 @@ __lastupdate__ = "February 2025"
 
 instr = 'pm10'
 
+from pathlib import Path
+import pandas as pd
+import settings as ts
+import single_instr_data_avail.tools as sida_tls
+
 
 def update_data_avail(instr):
-    import single_instr_data_avail.tools as sida_tls
-    import os
+    folder = Path(ts.basefolder) / f"thaao_{instr}"
+    file_path = folder / "Thule_2010_sampling_3mag23_modificato_per_data_availability.xls"
 
-    import numpy as np
-    import pandas as pd
+    # Read Excel file directly
+    pm10_tmp = pd.read_excel(file_path, index_col=0)
 
-    import settings as ts
+    # Create a DataFrame with True values for each date in pm10_tmp
+    pm10 = pd.DataFrame({"dt": pd.to_datetime(pm10_tmp.index), "mask": True})
 
-    date_list = pd.date_range(
-            ts.instr_metadata[instr]['start_instr'], ts.instr_metadata[instr]['end_instr'], freq='D').tolist()
-    folder = os.path.join(ts.basefolder, 'thaao_' + instr)
-
-    pm10_tmp = pd.DataFrame(columns=['dt', 'mask'])
-
-    fn = os.path.join(folder, 'Thule_2010_sampling_3mag23_modificato_per_data_availability.xls')
-    pm10_tmp = pd.read_excel(fn, index_col=0)
-    vals = np.repeat(True, len(pm10_tmp))
-
-    pm10 = pd.concat([pd.Series(pd.DatetimeIndex(pm10_tmp.index)), pd.Series(vals)], axis=1)
-
+    # Save data
     sida_tls.save_txt(instr, pm10)
+
