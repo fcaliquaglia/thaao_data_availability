74d4adceb7de74e13f8bc052afa6d1ff0b6a2967
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-02-27 18:20:59 +0100

    bugs

--- single_instr_data_avail/thaao_uv-vis_spec.py
+++ single_instr_data_avail/thaao_uv-vis_spec.py
@@ -32,9 +32,8 @@ def update_data_avail(instr):
     import settings as ts
 
     date_list = pd.date_range(
-            ts.instr_metadata[instr]['start_instr'], ts.instr_metadata[instr]['end_instr'], freq='M').tolist()
+            ts.instr_metadata[instr]['start_instr'], ts.instr_metadata[instr]['end_instr'], freq='ME').tolist()
     folder = os.path.join(ts.basefolder, "thaao_" + instr)
-
 
     uv_vis_spec = pd.DataFrame()
     for i in date_list:
@@ -55,24 +54,32 @@ def update_data_avail(instr):
         metadata = lines[:data_start]
 
         column0_names = [lines[9].strip()]
+        next_start = 11
 
         num_columns1 = int(lines[10].strip())
-        col1_multiplier = lines[11].strip()
-        col1_nan = lines[12].strip()
+        if num_columns1 <= 9:
+            col1_multiplier = lines[11].strip()
+            col1_nan = lines[12].strip()
+            next_start += 2
+        elif num_columns1 > 9:
+            col1_multiplier = lines[11].strip() + lines[12].strip()
+            col1_nan = lines[13].strip() + lines[14].strip()
+            next_start += 4
 
         column1_names = []
         column1_uom = []
-        for i in range(13, 13 + num_columns1):
+        for i in range(next_start, next_start + num_columns1):
             column1_names.append(lines[i].split(";")[0].strip())
             if ";" in lines[i]:
                 column1_uom.append(lines[i].split(";")[1].strip())
 
-        num_columns2 = int(lines[13 + num_columns1].strip())
-        col_multiplier2 = lines[13 + num_columns1 + 1].strip()
-        col_nan1 = lines[13 + num_columns1 + 2].strip()
+        num_columns2 = int(lines[next_start + num_columns1].strip())
+        col_multiplier2 = lines[next_start + num_columns1 + 1].strip()
+        col_nan1 = lines[next_start + num_columns1 + 2].strip()
+        next_start += 2
         column2_names = []
         column2_uom = []
-        for i in range(16 + num_columns1, 16 + num_columns1 + num_columns2):
+        for i in range(next_start + num_columns1 + 1, next_start + num_columns1 + num_columns2 + 1):
             column2_names.append(lines[i].split(";")[0].strip())
             if ";" in lines[i]:
                 column2_uom.append(lines[i].split(";")[1].strip())
@@ -94,20 +101,25 @@ def update_data_avail(instr):
 
             full_line = first_line + combined_lines
 
-
             if len(full_line) == len(df.columns):  # Ensure correct shape before adding
                 df.loc[len(df)] = full_line
             else:
                 print(f"Skipping row due to incorrect column count: {full_line}")
-        df['datetime'] = pd.to_datetime(df['year'].astype(str)) + pd.to_timedelta(df['Fractional julian day of the current year'].astype(float) - 1, unit='D')
-        df.set_index('datetime', inplace=True)
+        df['datetime'] = pd.to_datetime(df['year'].astype(str)) + pd.to_timedelta(
+                df[column0_names[0]].astype(float) - 1, unit='D')
 
-        uv_vis_spec=pd.concat([uv_vis_spec, df])
+        df.columns = df.columns.str.replace('/', '-', regex=False)
+        df.columns = df.columns.str.replace(' ', '_', regex=False)
+        try:
+            df['O3_vertical_density_(510_nm)'] = df['O3_vertical_ednsity_(510_nm)']
+            df.drop(columns=['O3_vertical_ednsity_(510_nm)'], inplace=True)
+        except KeyError as e:
+            pass
+        uv_vis_spec = pd.concat([uv_vis_spec, df], ignore_index=True)
 
-    df.drop(columns=['Fractional julian day of the current year', 'year', 'month number', 'day of month', 'hour', 'minute', 'datetime'], inplace=True)
-
-
-    # uv_vis_spec = pd.DataFrame(columns=['dt', 'mask'])  # uv_vis_spec_missing = pd.DataFrame(columns=['dt', 'mask'])
+    uv_vis_spec.set_index('datetime', inplace=True)
+    uv_vis_spec.drop(
+            columns=[column0_names[0], 'year', 'month number', 'day of month', 'hour', 'minute', 'datetime'],
+            inplace=True)
 
     sida_tls.save_mask_txt(uv_vis_spec, os.path.join(folder, "thaao_" + instr), instr)
-
