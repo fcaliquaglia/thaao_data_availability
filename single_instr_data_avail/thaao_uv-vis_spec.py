8347e5f676cc191fb55f85bfc672bd42c28aa5d4
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-02-28 09:10:54 +0100

    bugs

--- single_instr_data_avail/thaao_uv-vis_spec.py
+++ single_instr_data_avail/thaao_uv-vis_spec.py
@@ -31,6 +31,8 @@ def update_data_avail(instr):
 
     import settings as ts
 
+    # https://git.nilu.no/ebas/ebas-io/-/wikis/home#downloading-the-software
+
     date_list = pd.date_range(
             ts.instr_metadata[instr]['start_instr'], ts.instr_metadata[instr]['end_instr'], freq='ME').tolist()
     folder = os.path.join(ts.basefolder, "thaao_" + instr)
@@ -38,51 +40,76 @@ def update_data_avail(instr):
     uv_vis_spec = pd.DataFrame()
     for i in date_list:
         fn = os.path.join(folder, 'thtc' + i.strftime('%y%m') + '.erv')
+        fn_new = os.path.join(folder, 'thtc' + i.strftime('%y%m') + '_1.erv')
         if not os.path.exists(fn):
             continue
         with open(fn, 'r') as file:
             print(fn)
             lines = file.readlines()
+        with open(fn_new, 'w') as file:
+            file.writelines(lines[1:])
+        #
+        # # Open your NASA Ames 1010 file
+        # na_file = nappy.openNAFile(fn_new)
+        #
+        # # Retrieve the NASA Ames dictionary with all header and data information
+        # na_dict = na_file.getNADict()
+        #
+        # # Verify the file format index (should output 1010)
+        # print("File Format Index (FFI):", na_dict.get("FFI"))
+        #
+        # # List available variables and metadata
+        # print("Available variables:", na_file.getVariables())
+        #
+        # data = na_file.readData()
+        #
+        with open(fn_new, 'r') as file:
+            print(fn_new)
+            lines = file.readlines()
 
         # Identify number of header lines to skip (first element in line 1)
-        lines_to_skip = int(lines[1].split()[0])
+        lines_to_skip = int(lines[0].split()[0])
 
         # Identify where the data begins (after skipping header lines)
-        data_start = lines_to_skip + 1
+        data_start = lines_to_skip
 
         # Extract metadata (everything before the data start point)
         metadata = lines[:data_start]
 
-        column0_names = [lines[9].strip()]
-        next_start = 11
+        for i, line in enumerate(metadata):
+            if "julian day" in line.lower():  # Adjust this condition based on the file content
+                column0_names = [line.strip()]
 
-        num_columns1 = int(lines[10].strip())
+                break
+        next_start = 10
+
+        num_columns1 = int(metadata[9].strip())
         if num_columns1 <= 9:
-            col1_multiplier = lines[11].strip()
-            col1_nan = lines[12].strip()
+            col1_multiplier = metadata[10].strip()
+            col1_nan = metadata[11].strip()
             next_start += 2
         elif num_columns1 > 9:
-            col1_multiplier = lines[11].strip() + lines[12].strip()
-            col1_nan = lines[13].strip() + lines[14].strip()
+            col1_multiplier = metadata[10].strip() + metadata[11].strip()
+            col1_nan = metadata[12].strip() + metadata[13].strip()
             next_start += 4
 
         column1_names = []
         column1_uom = []
         for i in range(next_start, next_start + num_columns1):
-            column1_names.append(lines[i].split(";")[0].strip())
-            if ";" in lines[i]:
-                column1_uom.append(lines[i].split(";")[1].strip())
+            column1_names.append(metadata[i].split(";")[0].strip())
+            if ";" in metadata[i]:
+                column1_uom.append(metadata[i].split(";")[1].strip())
 
-        num_columns2 = int(lines[next_start + num_columns1].strip())
-        col_multiplier2 = lines[next_start + num_columns1 + 1].strip()
-        col_nan1 = lines[next_start + num_columns1 + 2].strip()
+        num_columns2 = int(metadata[next_start + num_columns1].strip())
+        col_multiplier2 = metadata[next_start + num_columns1 + 1].strip()
+        col_nan1 = metadata[next_start + num_columns1 + 2].strip()
         next_start += 2
         column2_names = []
         column2_uom = []
         for i in range(next_start + num_columns1 + 1, next_start + num_columns1 + num_columns2 + 1):
-            column2_names.append(lines[i].split(";")[0].strip())
-            if ";" in lines[i]:
-                column2_uom.append(lines[i].split(";")[1].strip())
+            column2_names.append(metadata[i].split(";")[0].strip())
+            if ";" in metadata[i]:
+                column2_uom.append(metadata[i].split(";")[1].strip())
 
         df = pd.DataFrame(columns=column0_names + column2_names + column1_names)
 
@@ -108,18 +135,61 @@ def update_data_avail(instr):
         df['datetime'] = pd.to_datetime(df['year'].astype(str)) + pd.to_timedelta(
                 df[column0_names[0]].astype(float) - 1, unit='D')
 
-        df.columns = df.columns.str.replace('/', '-', regex=False)
-        df.columns = df.columns.str.replace(' ', '_', regex=False)
         try:
-            df['O3_vertical_density_(510_nm)'] = df['O3_vertical_ednsity_(510_nm)']
-            df.drop(columns=['O3_vertical_ednsity_(510_nm)'], inplace=True)
+            df['O3_vertical density (510 nm)'] = df['O3 vertical ednsity (510 nm)']
+            df.drop(columns=['O3 vertical ednsity (510 nm)'], inplace=True)
         except KeyError as e:
             pass
-        uv_vis_spec = pd.concat([uv_vis_spec, df], ignore_index=True)
+        try:
+            df['NO2 vertical column density (430 nm)'] = df['NO2 vertical column_density (430 nm']
+            df.drop(columns=['NO2 vertical column density (430 nm'], inplace=True)
+        except KeyError as e:
+            pass
+        try:
+            df['Fractional julian day of the current year'] = df['Julian day of the current year']
+            df.drop(columns=['Fractional julian day of the current year'], inplace=True)
+        except KeyError as e:
+            pass
 
+        # Strip extra spaces from column names to avoid issues
+        df.columns = df.columns.str.strip()
+        # df.columns = df.columns.str.replace(' ', '_', regex=False)
+
+        # First, reset index to avoid index conflicts during concatenation
+        uv_vis_spec = uv_vis_spec.reset_index(drop=True)
+        df = df.reset_index(drop=True)
+
+        df.index = df.index + len(uv_vis_spec)
+        uv_vis_spec = uv_vis_spec.join(df, how='outer', rsuffix='_duplicate')
+
+    uv_vis_spec.columns = uv_vis_spec.columns.str.rstrip('_duplicate')
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("color", "colour")
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace(" : ", ": ")
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("O3_vertical", "O3 vertical")
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("(430 nm)", "(430 nm)")
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("O3_vertical", "O3 vertical")
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("datetim", "datetime")
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("air temperatur", "air temperature")
+
+    for col in uv_vis_spec.columns:
+        col_df2 = col + '_duplicate'
+        if col_df2 in uv_vis_spec.columns:
+            # Merge: take values from df1, and if NaN then use values from df2
+            uv_vis_spec[col] = uv_vis_spec[col].combine_first(uv_vis_spec[col_df2])
+            # Drop the extra column after merging
+            uv_vis_spec.drop(columns=[col_df2], inplace=True)
+
+    # uv_vis_spec = uv_vis_spec.groupby(axis=1, level=0).first()
+    uv_vis_spec = uv_vis_spec[uv_vis_spec.columns[~uv_vis_spec.columns.str.startswith('type of observation')]]
     uv_vis_spec.set_index('datetime', inplace=True)
-    uv_vis_spec.drop(
-            columns=[column0_names[0], 'year', 'month number', 'day of month', 'hour', 'minute', 'datetime'],
-            inplace=True)
 
+    for elem in ['Julian day of the current year', 'Julian day of the current year', 'year', 'day of month', 'hour',
+                 'min', 'month number']:
+        try:
+            uv_vis_spec.drop(columns=[elem], inplace=True)
+        except KeyError as e:
+            print(e)
+
+    import matplotlib.pyplot as plt
+    plt.plot(uv_vis_spec['air temperature'])
     sida_tls.save_mask_txt(uv_vis_spec, os.path.join(folder, "thaao_" + instr), instr)
