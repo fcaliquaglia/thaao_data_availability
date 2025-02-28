4a86d282ece0beafac77db5cdc6a84e5283bcda9
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-02-28 09:42:56 +0100

    bugs

--- single_instr_data_avail/thaao_uv-vis_spec.py
+++ single_instr_data_avail/thaao_uv-vis_spec.py
@@ -40,15 +40,9 @@ def update_data_avail(instr):
     uv_vis_spec = pd.DataFrame()
     for i in date_list:
         fn = os.path.join(folder, 'thtc' + i.strftime('%y%m') + '.erv')
-        fn_new = os.path.join(folder, 'thtc' + i.strftime('%y%m') + '_1.erv')
         if not os.path.exists(fn):
             continue
-        with open(fn, 'r') as file:
-            print(fn)
-            lines = file.readlines()
-        with open(fn_new, 'w') as file:
-            file.writelines(lines[1:])
-        #
+
         # # Open your NASA Ames 1010 file
         # na_file = nappy.openNAFile(fn_new)
         #
@@ -63,9 +57,10 @@ def update_data_avail(instr):
         #
         # data = na_file.readData()
         #
-        with open(fn_new, 'r') as file:
-            print(fn_new)
+        with open(fn, 'r') as file:
+            print(fn)
             lines = file.readlines()
+            lines = lines[1:]
 
         # Identify number of header lines to skip (first element in line 1)
         lines_to_skip = int(lines[0].split()[0])
@@ -77,41 +72,50 @@ def update_data_avail(instr):
         metadata = lines[:data_start]
 
         for i, line in enumerate(metadata):
-            if "julian day" in line.lower():  # Adjust this condition based on the file content
-                column0_names = [line.strip()]
+            if "julian" in line.lower():  # Adjust this condition based on the file content
+                col0_names = [line.strip()]
 
                 break
         next_start = 10
 
-        num_columns1 = int(metadata[9].strip())
-        if num_columns1 <= 9:
-            col1_multiplier = metadata[10].strip()
-            col1_nan = metadata[11].strip()
+        col1_num = int(metadata[9].strip())
+        if col1_num <= 9:
+            col1_multiplier = metadata[10].strip().split()
+            col1_multiplier = [float(x) for x in col1_multiplier]
+            col1_nan = metadata[11].strip().split()
+            col1_nan = [float(x) for x in col1_nan]
             next_start += 2
-        elif num_columns1 > 9:
-            col1_multiplier = metadata[10].strip() + metadata[11].strip()
-            col1_nan = metadata[12].strip() + metadata[13].strip()
+        elif col1_num > 9:
+            col1_multiplier = metadata[10].strip().split() + metadata[11].strip().split()
+            col1_multiplier = [float(x) for x in col1_multiplier]
+            col1_nan = metadata[12].strip().split() + metadata[13].strip().split()
+            col1_nan = [float(x) for x in col1_nan]
             next_start += 4
 
-        column1_names = []
-        column1_uom = []
-        for i in range(next_start, next_start + num_columns1):
-            column1_names.append(metadata[i].split(";")[0].strip())
+        col1_names = []
+        col1_uom = []
+        for i in range(next_start, next_start + col1_num):
+            col1_names.append(metadata[i].split(";")[0].strip())
             if ";" in metadata[i]:
-                column1_uom.append(metadata[i].split(";")[1].strip())
+                col1_uom.append(metadata[i].split(";")[1].strip())
 
-        num_columns2 = int(metadata[next_start + num_columns1].strip())
-        col_multiplier2 = metadata[next_start + num_columns1 + 1].strip()
-        col_nan1 = metadata[next_start + num_columns1 + 2].strip()
+        col2_num = int(metadata[next_start + col1_num].strip())
+        col2_multiplier = metadata[next_start + col1_num + 1].strip()
+        col2_multiplier = [float(x) for x in col2_multiplier.split()]
+        col2_nan = metadata[next_start + col1_num + 2].strip()
+        col2_nan = [float(x) for x in col2_nan.split()]
         next_start += 2
-        column2_names = []
-        column2_uom = []
-        for i in range(next_start + num_columns1 + 1, next_start + num_columns1 + num_columns2 + 1):
-            column2_names.append(metadata[i].split(";")[0].strip())
+        col2_names = []
+        col2_uom = []
+        for i in range(next_start + col1_num + 1, next_start + col1_num + col2_num + 1):
+            col2_names.append(metadata[i].split(";")[0].strip())
             if ";" in metadata[i]:
-                column2_uom.append(metadata[i].split(";")[1].strip())
+                col2_uom.append(metadata[i].split(";")[1].strip())
 
-        df = pd.DataFrame(columns=column0_names + column2_names + column1_names)
+        metadata_dict = {key: {"mult": mult, "nanval": nan, "uom": uom} for key, mult, nan, uom, in
+                         zip(col1_names, col1_multiplier, col1_nan, col1_uom)}
+
+        df = pd.DataFrame(columns=col0_names + col2_names + col1_names)
 
         for i in range(data_start, len(lines) - 1):
             first_line = lines[i].split()
@@ -119,11 +123,11 @@ def update_data_avail(instr):
 
             # Ensure combined_lines has the correct number of columns
             k = 1  # Start at the next line
-            while len(combined_lines) < num_columns1 and i + k + 1 < len(lines):
+            while len(combined_lines) < col1_num and i + k + 1 < len(lines):
                 combined_lines.extend(lines[i + k + 1].split())
                 k += 1
 
-            if len(combined_lines) != num_columns1:
+            if len(combined_lines) != col1_num:
                 continue  # Skip if still incorrect
 
             full_line = first_line + combined_lines
@@ -133,7 +137,7 @@ def update_data_avail(instr):
             else:
                 print(f"Skipping row due to incorrect column count: {full_line}")
         df['datetime'] = pd.to_datetime(df['year'].astype(str)) + pd.to_timedelta(
-                df[column0_names[0]].astype(float) - 1, unit='D')
+                df[col0_names[0]].astype(float) - 1, unit='D')
 
         try:
             df['O3_vertical density (510 nm)'] = df['O3 vertical ednsity (510 nm)']
@@ -166,7 +170,8 @@ def update_data_avail(instr):
     uv_vis_spec.columns = uv_vis_spec.columns.str.replace("color", "colour")
     uv_vis_spec.columns = uv_vis_spec.columns.str.replace(" : ", ": ")
     uv_vis_spec.columns = uv_vis_spec.columns.str.replace("O3_vertical", "O3 vertical")
-    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("(430 nm)", "(430 nm)")
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("(430 nm)", "(430 nm")
+    uv_vis_spec.columns = uv_vis_spec.columns.str.replace("(430 nm", "(430 nm)")
     uv_vis_spec.columns = uv_vis_spec.columns.str.replace("O3_vertical", "O3 vertical")
     uv_vis_spec.columns = uv_vis_spec.columns.str.replace("datetim", "datetime")
     uv_vis_spec.columns = uv_vis_spec.columns.str.replace("air temperatur", "air temperature")
@@ -179,17 +184,27 @@ def update_data_avail(instr):
             # Drop the extra column after merging
             uv_vis_spec.drop(columns=[col_df2], inplace=True)
 
-    # uv_vis_spec = uv_vis_spec.groupby(axis=1, level=0).first()
+    uv_vis_spec = uv_vis_spec.T.groupby(level=0).first().T
     uv_vis_spec = uv_vis_spec[uv_vis_spec.columns[~uv_vis_spec.columns.str.startswith('type of observation')]]
     uv_vis_spec.set_index('datetime', inplace=True)
 
-    for elem in ['Julian day of the current year', 'Julian day of the current year', 'year', 'day of month', 'hour',
-                 'min', 'month number']:
+    for elem in ['Julian day of the current year', 'Fractional julian day of the current year', 'year', 'day of month',
+                 'hour', 'min', 'month number']:
         try:
             uv_vis_spec.drop(columns=[elem], inplace=True)
         except KeyError as e:
             print(e)
 
+    for icol in uv_vis_spec.columns:
+        try:
+            uv_vis_spec[icol].replace(metadata_dict[icol]['nanval'], pd.NA, inplace=True)
+        except KeyError as e:
+            print(e)
+
     import matplotlib.pyplot as plt
-    plt.plot(uv_vis_spec['air temperature'])
+    plt.plot(
+        uv_vis_spec['NO2 vertical column density (430 nm)'] * metadata_dict['NO2 vertical column density (430 nm)'][
+            'mult'])
+    plt.ylabel(metadata_dict['NO2 vertical column density (430 nm)']['uom'])
+
     sida_tls.save_mask_txt(uv_vis_spec, os.path.join(folder, "thaao_" + instr), instr)
