9bdb568cfdfb04ca962a50916cc355e105903071
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-03-02 21:43:51 +0100

    ftir ceilometer ecapac_aws_snow metar uv-vis_spec pm10 aeronet vespa rad_dli rad_dsi rad_usi rad_uli rad_alb rad_tpb rad_par_up rad_par_down

--- plots.py
+++ plots.py
@@ -80,7 +80,7 @@ def draw_data_summary():
 
         # Primary Y-Axis (Left)
         ax.plot(data_filtered.index, data_filtered[var_], color=color, marker='o', markersize=2, linestyle='-', label=instr)
-        ax.set_ylabel(f"{lab} [{uom}]", color=color, fontsize=10, fontweight='bold')
+        ax.set_ylabel(f"{lab} \n [{uom}]", color=color, fontsize=10, fontweight='bold')
         ax.tick_params(axis='y', colors=color, labelsize=8)
         ax.patch.set_facecolor('lightgrey')
 
