a7face2777de043a1de5e1e6e69853f83ee387e5
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-03-03 15:17:12 +0100

    hyso_tide_1 hatpro rs_sondes ftir ceilometer ecapac_aws_snow metar uv-vis_spec pm10 aeronet vespa rad_dli rad_dsi rad_usi rad_uli rad_alb rad_tbp rad_par_up rad_par_down wv_isotopes

--- single_instr_data_avail/thaao_wv_isotopes.py
+++ single_instr_data_avail/thaao_wv_isotopes.py
@@ -3,16 +3,17 @@
 # -------------------------------------------------------------------------------
 #
 """
-OK
+Optimized code for updating data availability for isotopic data.
 """
 
 # =============================================================
-# CREATED: 
+# CREATED:
 # AFFILIATION: INGV
 # AUTHORS: Filippo Cali' Quaglia
 # =============================================================
 #
 # -------------------------------------------------------------------------------
+
 __author__ = "Filippo Cali' Quaglia"
 __credits__ = ["??????"]
 __license__ = "GPL"
@@ -21,50 +22,46 @@ __email__ = "filippo.caliquaglia@ingv.it"
 __status__ = "Research"
 __lastupdate__ = "February 2025"
 
-instr = 'wv_isotopes'
+import os
+import pandas as pd
+import settings as ts
+import single_instr_data_avail.sida_tools as sida_tls
 
+# Helper function to load and process each file
+def load_isotope_data(file_path, datetime_col, date_format, data_col):
+    data = pd.read_csv(file_path)
+    data['datetime'] = pd.to_datetime(data[datetime_col], format=date_format)
+    data.set_index('datetime', inplace=True)
+    data['d18O'] = data[data_col]  # Add the isotopic data
+    return data[['d18O']]
 
 def update_data_avail(instr):
-    import os
-
-    import single_instr_data_avail.sida_tools as sida_tls
-    import pandas as pd
-
-    import settings as ts
-
-    date_list = pd.date_range(
-            ts.instr_metadata[instr]['start_instr'], ts.instr_metadata[instr]['end_instr'], freq='D').tolist()
     folder = os.path.join(ts.basefolder, "thaao_" + instr)
 
-    wv_isotopes = pd.DataFrame(columns=['dt', 'mask'])
-    wv_isotopes_missing = pd.DataFrame(columns=['dt', 'mask'])
+    # Load all isotope data using the helper function
+    wv_isotopes1 = load_isotope_data(
+        os.path.join(folder, 'BGK_MOSAiC_ADC_finaldataset.csv'),
+        'DateTime_UTC', '%m/%d/%Y %H:%M', 'BAR_d18O'
+    )
 
-    fn1 = os.path.join(folder, 'BGK_MOSAiC_ADC_finaldataset.csv')
-    wv_isotopes1 = pd.read_csv(fn1)
-    wv_isotopes1['DateFormatted'] = pd.to_datetime(wv_isotopes1['DateTime_UTC'], format='%m/%d/%Y %H:%M')  # .round('h')
+    wv_isotopes2 = load_isotope_data(
+        os.path.join(folder, 'thule_isotope_wx_10min_2017_2019.csv'),
+        'min10break', '%d-%m-%y %H:%M', None  # No 'd18O' column for this file
+    )
 
-    fn2 = os.path.join(folder, 'thule_isotope_wx_10min_2017_2019.csv')
-    wv_isotopes2 = pd.read_csv(fn2)
-    wv_isotopes2['DateFormatted'] = pd.to_datetime(wv_isotopes2['min10break'], format='%d-%m-%y %H:%M')
+    wv_isotopes3 = load_isotope_data(
+        os.path.join(folder, 'Thule_water_vapor_isotopes_16.csv'),
+        'Date', '%Y-%m-%d %H:%M:%S', 'd18o'
+    )
 
-    fn3 = os.path.join(folder, 'Thule_water_vapor_isotopes_16.csv')
-    wv_isotopes3 = pd.read_csv(fn3)
-    wv_isotopes3['DateFormatted'] = pd.to_datetime(wv_isotopes3['Date'], format='%Y-%m-%d %H:%M:%S')
+    wv_isotopes4 = load_isotope_data(
+        os.path.join(folder, 'ThuleAFB_ppt_isotopes.csv'),
+        'Date ', '%m/%d/%y', 'd18O (per mil)'
+    )
 
-    fn4 = os.path.join(folder, 'ThuleAFB_ppt_isotopes.csv')
-    wv_isotopes4 = pd.read_csv(fn4)
-    wv_isotopes4['DateFormatted'] = pd.to_datetime(wv_isotopes4['Date '], format='%m/%d/%y')
+    # Concatenate all dataframes and sort by datetime index
+    wv_isotopes = pd.concat([wv_isotopes1, wv_isotopes2, wv_isotopes3, wv_isotopes4])
+    wv_isotopes.sort_index(inplace=True)
 
-    wv_isotopes_dt = pd.DataFrame(
-            pd.concat(
-                    [wv_isotopes1['DateFormatted'], wv_isotopes2['DateFormatted'], wv_isotopes3['DateFormatted'],
-                     wv_isotopes4['DateFormatted']]))
-    wv_isotopes_dt.index = pd.DatetimeIndex(wv_isotopes_dt['DateFormatted'])
-
-    for i in date_list:
-        if i in wv_isotopes_dt.index:
-            wv_isotopes.loc[i] = [i, True]
-        else:
-            wv_isotopes_missing.loc[i] = [i, True]
-
+    # Save the processed data
     sida_tls.save_csv(instr, wv_isotopes)
