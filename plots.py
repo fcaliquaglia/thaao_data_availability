54750ff7a5dfc740386e75627cbe012ef2912583
Filippo Calì Quaglia <filippo.caliquaglia@gmail.com>
2025-03-08 18:52:31 +0100

    ftir lidar_temp hatpro rs_sondes ceilometer ecapac_aws_snow metar uv-vis_spec pm10 aeronet vespa rad_dli rad_dsi rad_usi rad_uli rad_alb rad_tbp rad_par_up rad_par_down wv_isotopes

--- plots.py
+++ plots.py
@@ -42,8 +42,8 @@ def draw_data_summary():
         data_all.loc[data_all['hatpro__IWV'] > 50, 'hatpro__IWV'] = np.nan
     if 'hatpro__LWP_gm-2' in data_all.columns:
         data_all.loc[data_all['hatpro__LWP_gm-2'] > 1000, 'hatpro__LWP_gm-2'] = np.nan
-    if 'ftir__ch4-2' in data_all.columns:
-        data_all.loc[data_all['ftir__ch4'] < 3.3E-19, 'ftir__ch4-2'] = np.nan
+    if 'ftir__ch4' in data_all.columns:
+        data_all.loc[data_all['ftir__ch4'] < 3.3E-19, 'ftir__ch4'] = np.nan
     var_list = []
     for instr in ts.instr_list:
         var_list += [instr + '__' + j for j in list(ts.instr_metadata[instr]['plot_vars'].keys())]
